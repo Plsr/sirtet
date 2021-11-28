6d156c86fd563a84fae385fa4892cc372551607b
Plsr <christianpoplawski@gmail.com>
2021-11-29 00:11:13 +0100

    Add all other blocks

--- Main.gd
+++ Main.gd
@@ -1,11 +1,29 @@
 extends Node
 
-export (PackedScene) var Block
+""""
+const OBlock = preload("res://Blocks/OBlock.tscn")
+const TBlock = preload("res://Blocks/TBlock.tscn")
+const LBlock = preload("res://Blocks/LBlock.tscn")
+const JBlock = preload("res://Blocks/JBlock.tscn")
+const SBlock = preload("res://Blocks/SBlock.tscn")
+const ZBlock = preload("res://Blocks/ZBlock.tscn")
+const IBlock = preload("res://Blocks/IBlock.tscn")
+"""
+var _available_blocks = {
+	OBlock = preload("res://Blocks/OBlock.tscn"),
+	TBlock = preload("res://Blocks/TBlock.tscn"),
+	LBlock = preload("res://Blocks/LBlock.tscn"),
+	JBlock = preload("res://Blocks/JBlock.tscn"),
+	SBlock = preload("res://Blocks/SBlock.tscn"),
+	ZBlock = preload("res://Blocks/ZBlock.tscn"),
+	IBlock = preload("res://Blocks/IBlock.tscn")
+}
 
 const TICK_SPEED_MS = 1000
 const CELL_SIZE     = 40
 
 var _current_block = null
+var _current_block_rotation_deg = 0
 
 # DEBUG SECTION
 # TODO: Make toggable via menu
@@ -14,9 +32,11 @@ var _DEBUG_up_move_enabled = false
 var _DEBUG_shop_collision_hints = false
 
 func _ready():
-	print($BlockArea/BlockMoveArea)
-	_current_block = Block.instance()
-	
+	randomize()
+	var random_block = _available_blocks.keys()[randi() % _available_blocks.size()]
+	print(randi() % _available_blocks.size())
+	_current_block = _available_blocks[random_block].instance()
+
 	add_child(_current_block)
 	_current_block.position = $BlockSpawn/BlockSpawnArea.position
 	
@@ -31,19 +51,19 @@ func _on_BlockMoveTimer_timeout():
 	if _current_block_can_move(Vector2.DOWN):
 		_current_block.position.y += CELL_SIZE
 
-func _process(delta):
+func _process(_delta):
 	pass
 
 
 func _current_block_can_move(direction):
 	var ray = _current_block.get_node("Ray")
-	var block_widht = _current_block.get_node("CollisionShape2D").shape.extents.x
-	ray.cast_to = direction * (CELL_SIZE + block_widht)
+	var rad_rotation = deg2rad(_current_block_rotation_deg)
+	ray.cast_to = (direction * (CELL_SIZE * 2)).rotated(-rad_rotation)
 	ray.force_raycast_update()
 	return !ray.is_colliding()
 
 
-func _input(event):
+func _input(_event):
 	if Input.is_key_pressed(KEY_LEFT):
 		if(_current_block_can_move(Vector2.LEFT)):
 			_current_block.position.x -= CELL_SIZE
@@ -54,5 +74,18 @@ func _input(event):
 		if(_current_block_can_move(Vector2.DOWN)):
 			_current_block.position.y += CELL_SIZE
 	if Input.is_key_pressed(KEY_UP):
-		if(_current_block_can_move(Vector2.UP) && _DEBUG_up_move_enabled):
-			_current_block.position.y -= CELL_SIZE
+		if _DEBUG_up_move_enabled:
+			if(_current_block_can_move(Vector2.UP)):
+				_current_block.position.y -= CELL_SIZE
+	if Input.is_key_pressed(KEY_SPACE):
+		# TODO: Validate rotation is possible
+		# TODO: Wall evasion
+		_current_block.set_rotation_degrees(_update_rotation())
+
+func _update_rotation():
+	if _current_block_rotation_deg == 270:
+		_current_block_rotation_deg = 0
+	else:
+		_current_block_rotation_deg += 90
+	
+	return _current_block_rotation_deg
--- Shape.gd
+++ Shape.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,6 +8,9 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("block instanciated")
+	var sprites = get_tree().get_nodes_in_group("Sprites")
+	for sprite in sprites:
+		sprite.modulate = Color.limegreen
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
